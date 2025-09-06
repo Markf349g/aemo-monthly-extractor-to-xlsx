346e852f9f648d16e7c290d0ca94c71198812da7
Mark V <mark.vizzhachiy@gmail.com>
2025-09-06 22:56:24 +0700

    Distribution Update

--- aemo_monthly_extractor_to_xlsx[win10-amd64].ps1
+++ aemo_monthly_extractor_to_xlsx[win10-amd64].ps1
@@ -1,8 +1,3 @@
 # === Windows 10 or newer ===
-
-$PY_PATH = Join-Path $PSScriptRoot "python-3.13.7-embed-amd64\python.exe"
-$UV_PATH = Join-Path $PSScriptRoot "uv-x86_64-pc-windows-msvc\uv.exe"
-$env:PATH = "$PY_PATH;$env:PATH"
-& $UV_PATH run aemo_monthly_extractor_to_xlsx.py
-
+& "uv-x86_64-pc-windows-msvc\uv.exe" run aemo_monthly_extractor_to_xlsx.py
 # How To Start: powershell -ExecutionPolicy Bypass -File aemo_monthly_extractor_to_xlsx[win10-amd64].ps1
